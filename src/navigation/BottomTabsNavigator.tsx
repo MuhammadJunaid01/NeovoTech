e4d5c525460c334db0a8659566bdd63ee37a3447
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2025-01-13 13:16:06 +0600

    Implement bottom tab navigation and add Profile screen; remove old navigators

---
+++ src/navigation/BottomTabsNavigator.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/no-unstable-nested-components */
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+import ActivitiesScreen from '../screens/ActivitiesScreen';
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabsNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarIcon: ({color, size}) => {
+          let iconName: string;
+
+          if (route.name === 'Home') {
+            iconName = 'home-outline';
+          } else if (route.name === 'Activities') {
+            iconName = 'list-outline';
+          } else if (route.name === 'Profile') {
+            iconName = 'person-outline';
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#2c3e50',
+        tabBarInactiveTintColor: '#95a5a6',
+        tabBarStyle: {backgroundColor: '#ecf0f1', paddingBottom: 5},
+      })}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Activities" component={ActivitiesScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabsNavigator;
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,5 +1,6 @@
 import ActivitiesScreen from './ActivitiesScreen';
 import DetailsScreen from './DetailsScreen';
 import HomeScreen from './HomeScreen';
+import ProfileScreen from './ProfileScreen';
 
-export {ActivitiesScreen, DetailsScreen, HomeScreen};
+export {ActivitiesScreen, DetailsScreen, HomeScreen, ProfileScreen};
