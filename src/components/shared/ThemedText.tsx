a6f3b7db6de3f97fcaa1e7ae9b0e03e3a425efd2
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2025-01-13 13:02:33 +0600

    Add initial component and interface structure with Tailwind integration

---
+++ src/components/shared/ThemedText.tsx
@@ -0,0 +1,80 @@
+import React, {memo, useCallback} from 'react';
+import {Text, TextProps} from 'react-native';
+import tw from '../../../tailwind';
+
+type Variant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+interface IProps extends TextProps {
+  variant: Variant;
+  color?: string;
+}
+
+const ThemedText: React.FC<IProps> = ({
+  variant,
+  children,
+  color = 'text-[#374151]',
+  style,
+  ...rest
+}) => {
+  const textStyle = useCallback(() => {
+    let styleIn = {};
+    switch (variant) {
+      case 'h1':
+        styleIn = {
+          fontSize: 24,
+          fontWeight: 'bold' as 'bold',
+        };
+        break;
+      case 'h2':
+        styleIn = {
+          fontSize: 19,
+          fontWeight: 'bold' as 'bold',
+        };
+        break;
+      case 'h3':
+        styleIn = {
+          fontSize: 17,
+          fontWeight: '600' as '600',
+        };
+        break;
+      case 'h4':
+        styleIn = {
+          fontSize: 14,
+          lineHeight: 21,
+          fontWeight: '500' as '500',
+        };
+        break;
+      case 'h5':
+        styleIn = {
+          fontSize: 13,
+          fontWeight: '400' as '400',
+        };
+        break;
+      case 'h6':
+        styleIn = {
+          fontSize: 11,
+          fontWeight: '300' as '300',
+        };
+        break;
+      default:
+        styleIn = {};
+        break;
+    }
+
+    // Apply dynamic text color here
+    return {
+      ...styleIn,
+      ...(typeof style === 'object' ? style : {}),
+    };
+  }, [style, variant]);
+
+  const combinedStyle = textStyle();
+
+  return (
+    <Text style={[combinedStyle, tw`${color}`]} {...rest}>
+      {children}
+    </Text>
+  );
+};
+
+export default memo(ThemedText);
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4F4F4F',
+        secondary: '#50586D',
+        info: '#B2DB00',
+        darkCyan: '#4BB543',
+      },
+    },
+  },
+  plugins: [],
+};
